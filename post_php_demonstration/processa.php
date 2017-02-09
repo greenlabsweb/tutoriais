f4fe973bd2417ed7ae8bacae1df1966f1f97574a
greenlabsweb <matheus@greenlabsweb.com.br>
2017-02-09 08:36:01 -0200

    Signed-off-by: Matheus <matheus@greenlabsweb.com.br> Signed-off-by: greenlabsweb <matheus@greenlabsweb.com.br>

---
+++ post_php_demonstration/processa.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$uf = $_POST["uf"];
+
+?>
+
+Nome: <?php echo $nome." //"; ?>
+ E-mail: <?php echo $email." //"; ?>
+ Estado: <?php echo $uf; ?>
